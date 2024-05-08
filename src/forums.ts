f07df75e523c240879a540458c91f8be036554fe
Italo Castillo <italogitdoe@gmail.com>
2024-05-08 23:48:33 +0200

    add more endpoints

--- src/forums.ts
+++ src/forums.ts
@@ -1,6 +1,7 @@
 import type { Express } from "express";
 import { db } from "./db";
 import { Router } from "express";
+import { NotFoundError } from "@prisma/client/runtime/library";
 
 const router = Router();
 
@@ -23,10 +24,47 @@ router.get("/", async (request, response) => {
         forumId: true,
       },
     });
-
     response.status(200).json(forums);
   } catch (error) {
     response.status(500).json({ error: "Internal Error" });
+  }
+});
+
+router.post("/", async (request, response) => {
+  try {
+    const { name } = request.body;
+    if (name === undefined || typeof name !== "string")
+      response.status(400).json({ error: "Wrong name field" });
+
+    const forum = await db.forum.create({
+      data: {
+        name,
+      },
+    });
+    response.status(201).json(forum);
+  } catch (error) {
+    response.status(500).json({ error: "Internal Error" });
+  }
+});
+
+router.get("/:id", async (request, response) => {
+  try {
+    var { id } = request.params;
+
+    const forum = await db.forum.findUniqueOrThrow({
+      where: {
+        forumId: Number(id),
+      },
+    });
+
+    response.status(200).json(forum);
+  } catch (e: any) {
+    if (e.name === "NotFoundError") {
+      response.status(404).json({ message: "Not Found" });
+    } else if (e.name === "PrismaClientValidationError") {
+      response.status(404).json({ message: "Field not valid" });
+    }
+    response.status(500).json({ name: e.name, error: "Internal Error" });
   }
 });
 
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json());
+app.use(express.json()); //parse req to json
 
 
 app.use("/forums", forumRoutes)

13443ac1202a13beb03a241c41ea5e536e800352
Italo Castillo <italogitdoe@gmail.com>
2024-05-08 23:20:55 +0200

    add router

--- src/forums.ts
+++ src/forums.ts
@@ -1,32 +1,33 @@
 import type { Express } from "express";
 import { db } from "./db";
+import { Router } from "express";
 
+const router = Router();
 
 /*
 GET     /forums/
-POST    /forums
+POST    /forums/
 GET     /forums/:id
 PUT     /forums/:id
 DELETE  /forums/:id
-
 */
 
-export const setupForumEndPoints = (app: Express) => {
-  app.get("/forums", async (request, response) => {
-    try {
-      const forums = await db.forum.findMany({
-        orderBy: {
-          createdAt: "asc",
-        },
-      });
+router.get("/", async (request, response) => {
+  try {
+    const forums = await db.forum.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+      select: {
+        name: true,
+        forumId: true,
+      },
+    });
 
-      response.status(200).json(forums);
-    } catch (error) {
-      response.status(500).json({ error: "Internal Error" });
-    }
-  });
+    response.status(200).json(forums);
+  } catch (error) {
+    response.status(500).json({ error: "Internal Error" });
+  }
+});
 
-  app.get("/", async (request, response) => {
-    response.status(200).json({ ok: true, message: "hey, !2!" });
-  });
-};
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import { db } from "./db";
-import { setupForumEndPoints } from "./forums";
+import forumRoutes from "./forums"
 
 const app = express();
 
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-setupForumEndPoints(app);
+
+app.use("/forums", forumRoutes)
 
 // const PORT = process.env.PORT;
 const { PORT } = process.env;
